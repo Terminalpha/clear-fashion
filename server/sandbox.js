4556c820ffe6cdba5e4d89aeccf4cd5a2cafef7c
Terminalpha <Terminalpha@users.noreply.github.com>
2021-02-01 17:09:00 +0100

    suite

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
-
+const adressebrand = require('./sources/adressebrand');
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
@@ -18,4 +18,22 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 
 const [,, eshop] = process.argv;
 
-sandbox(eshop);
+//sandbox(eshop);
+//sandbox('https://mudjeans.eu/collections/men-buy-jeans');
+sandbox('https://adresse.paris/602-nouveautes');
+
+async function stylebox (eshop = 'https://adresse.paris/602-nouveautes') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+
+    const products = await adressebrand.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+stylebox(eshop);
