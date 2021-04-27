7a144b9f3b75b0692fd504ab5755fd02ea7ae951
Terminalpha <Terminalpha@users.noreply.github.com>
2021-04-27 22:56:32 +0200

    Update portfolio.js

--- client/portfolio.js
+++ client/portfolio.js
@@ -38,7 +38,7 @@ const fetchProducts = async (page = 1, size = 12) => {
   try {
     
     const response = await fetch(
-      `https://server-ashy.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-seven.vercel.app/?page=${page}&size=${size}`
     );
     const body = await response.json();
 
